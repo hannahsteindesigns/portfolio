68977c2c18b2372a7b9d1ce6a8a5a0bb5ec210ba
Hannah <hannahsteindesigns@gmail.com>
2017-11-29 19:06:57 -0600

    adding site styles and images, needs to be changed later to be DRY

---
+++ assets/js/scripts.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+  // mobile nav
+  $(".mobile-nav").click(function(){
+    $(this).toggleClass("active");
+    $("nav").slideToggle()
+  });
+
+  /* contact form */
+  // scroll to form
+  $("a[title='contact']").click(function(e){
+    e.preventDefault();
+    var target = this.hash,
+        $target = $(target);
+    $(".mobile-nav").trigger("click");
+    $("html, body").stop().animate({
+      "scrollTop": $target.offset().top
+    }, 900, "swing", function(){
+      history.pushState(null, null, target);
+    });
+  });
+  // keep adaptive placeholders in place
+  $("input, textarea").change(function(){
+    var el = $(this);
+    el.val().trim().length ? el.addClass("focus") : el.removeClass("focus");
+  });
+  // initialize validation
+  $("#form").validate({
+    errorElement: "div",
+    errorPlacement: function(error, element) {
+      error.appendTo( element.parent("div") );
+    },
+    showErrors: function(errorMap, errorList) {
+      this.defaultShowErrors();
+      $.each(errorMap, function(key, value) {
+        var error = $("input[name='" + key + "']").nextAll("div");
+        if (error.text() !== value) {
+          error.html(value);
+        }
+      });
+    },
+    submitHandler: function(form, event) {
+      submitForm(form);
+    }
+  });
+  // submit form logic
+  var submitForm = function(form) {
+    var formMessages = $("#form-messages"),
+        formData = $(form).serialize();
+		$.ajax({
+			type: "POST",
+			url: $(form).attr("action"),
+			data: formData
+		})
+		.done(function(response) {
+			$(formMessages).removeClass("error").addClass("success");
+      $("input, textarea").removeClass("focus");
+			$(formMessages).hide().text(response).fadeIn();
+			$('#name, #email, #message').val("");
+      $(form).validate().resetForm();
+      setTimeout(function(){
+        $(formMessages).hide("slow");
+      }, 10000);
+		})
+		.fail(function(data) {
+			$(formMessages).removeClass("success").addClass("error");
+			if (data.responseText !== "") {
+				$(formMessages).hide().text(data.responseText).fadeIn();
+			} else {
+				$(formMessages).hide().text("Oops! Something went wrong and we couldn't send your message. Please try again.").fadeIn();
+			}
+		});
+  };
+});
---
+++ site/templates/home.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html>
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52747973-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-52747973-1');
+    </script>
+    <title>hannah stein designs</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, initial-scale=1, maximum-scale=2, user-scalable=yes">
+    <!-- styles & images -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Roboto:400,700" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
+    <!-- <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../favicon.ico" type="image/x-icon"> -->
+    <!-- scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
+  </head>
+
+  <body>
+    <!-- header -->
+    <header>
+      <div class="width">
+        <h1><a href="#">Hannah Stein</a></h1>
+          <a class="mobile-nav" title="toggle menu"><span></span></a>
+          <nav>
+            <a href="/" title="portfolio" class="active">portfolio</a>
+            <a href="dev.hannahsteindesigns.com/about.html" title="about">about</a>
+            <a href="#contact" title="contact">contact</a>
+          </nav>
+      </div>
+      </header>
+      <!-- content -->
+      <section id="content" class="width">
+        <div class="row-wrapper">
+          <div class="outer-container">
+            <article class="teaser--top">
+              <div class="meta">Mar 10, 2015 by Ralph Grando</div>
+              <h1>The Chevron</h1>
+              <div class="teaser-image"></div>
+              <p class="teaser-content">It's fair to say that The Chevron ruled the culture of the 1980s. No less a figure than Tom Selleck himself made it popular in our all-time favorite TV series Magnum. In this edition, we'll take a closer look at this fine portion of facial hair.</p>
+              <p class="continue-reading"><a href="detail-view.html">Continue Reading</a></p>
+            </article>
+          </div>
+        </div>
+      </section>
+      <!-- footer -->
+      <footer id="contact">
+        <div class="width">
+          <section>
+            <div class="social">
+              <h2>Let's talk.</h2>
+              <a href="mailto:&#104;&#97;&#110;&#110;&#97;&#104;&#115;&#116;&#101;&#105;&#110;&#100;&#101;&#115;&#105;&#103;&#110;&#115;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;" title="email me">
+                <img src="assets/images/email.svg" width="24" />
+                <span>hannahsteindesigns@gmail.com</span>
+              </a>
+              <a href="https://www.linkedin.com/in/hannahsteindesigns" title="connect on linkedin">
+                <img src="assets/images/linkedin.svg" width="24" />
+                <span>linkedin.com/in/hannahsteindesigns</span>
+              </a>
+              <a href="https://www.behance.net/hannahsteindesigns" title="see more of my work on behance">
+                <img src="assets/images/behance.svg" width="24" />
+                <span>behance.net/hannahsteindesigns</span>
+              </a>
+              <a href="https://issuu.com/hannahsteindesigns" title="see more of my print work on issuu">
+                <img src="assets/images/issuu.svg" width="24" />
+                <span>issuu.com/hannahsteindesigns</span>
+              </a>
+            </div>
+            <div class="form">
+              <div id="form-messages"></div>
+              <form name="hscontact" id="form" method="post" action="contact.php" novalidate>
+                <div class="name-input">
+                  <input required type="text" name="name" id="name">
+                  <label for="name" placeholder="Name"></label>
+                </div>
+                <div class="email-input">
+                  <input required type="email" name="email" id="email">
+                  <label for="email" placeholder="Email"></label>
+                </div>
+                <div class="message-input">
+                  <textarea required name="message" id="message"></textarea>
+                  <label for="message" placeholder="Send me a message"></label>
+                </div>
+                <button type="submit" id="submit" class="full">Send</button>
+              </form>
+            </div>
+          </section>
+        </div>
+      </footer>
+      <script src="assets/js/scripts.js"></script>
+  </body>
+</html>
