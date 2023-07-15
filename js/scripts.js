f72e78d2e5a4d46d5b5918bad071da1402468f7d
Hannah Stein <hannahsteindesigns@gmail.com>
2023-07-15 00:18:12 -0500

    removing outdated content

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
   // fade in pretty
   $("#content").css("opacity", "1");
 
+
+  // slide up
+
+
+
+
   // mobile nav
   $(".mobile-nav").click(function(){
     $(this).toggleClass("active");
@@ -9,10 +15,10 @@ $(document).ready(function(){
   });
 
   /* scroll to top */
-  var scrolling = false,
-      showArrow = function(){
-        ($(window).scrollTop() > 300) ? $("#scrollTop").fadeIn() : $("#scrollTop").fadeOut();
-      };
+  let scrolling = false;
+  const showArrow = function(){
+          $(window).scrollTop() > 300 ? $("#scrollTop").fadeIn() : $("#scrollTop").fadeOut();
+        };
   $(window).scroll(function(){ scrolling = true; });
   setInterval(function(){
     if (scrolling) {
@@ -34,12 +40,12 @@ $(document).ready(function(){
 
   /* progressive image loading */
   // adapted from https://www.sourcetoad.com/dev-trends/progressive-loading-of-images/
-  var lazyLoad = function(){
+  const lazyLoad = function(){
     $(".progressive").each(function(){
-      var container = $(this),
-          image = new Image(),
-          previewImage = $(this).find(".lowres"),
-          newImage = $("<img src='' class='overlay' alt='' />")
+      let container = $(this);
+      let image = new Image();
+      let previewImage = $(this).find(".lowres");
+      let newImage = $("<img src='' class='overlay' alt='' />");
       image.onload = function(){
         newImage.attr("src", image.src).attr("alt", previewImage.attr("alt")).appendTo(container).css("opacity", "1");
         previewImage.css("opacity", "0");
@@ -56,18 +62,18 @@ $(document).ready(function(){
 
   /* animations */
   // adapted from https://www.sitepoint.com/scroll-based-animations-jquery-css3/
-  var animate = $('.animate article'),
-      $window = $(window),
-      checkView = function() {
-        var windowHeight = $window.height(),
-            windowTop = $window.scrollTop(),
-            windowBottom = ( windowTop + windowHeight + 200 );
+  const animate = $('.animate article');
+  const $window = $(window)
+  const checkView = function() {
+        let windowHeight = $window.height();
+        let windowTop = $window.scrollTop();
+        let windowBottom = windowTop + windowHeight + 200;
 
         $.each(animate, function(){
-          var el = $(this),
-              elHeight = el.outerHeight(),
-              elTop = el.offset().top,
-              elBottom = ( elTop + elHeight );
+          let el = $(this);
+          let elHeight = el.outerHeight();
+          let elTop = el.offset().top;
+          let elBottom = ( elTop + elHeight );
 
           if ( (elBottom >= windowTop) && (elTop <= windowBottom) ) {
             el.addClass("in-view");
@@ -83,83 +89,17 @@ $(document).ready(function(){
       };
 
       checkView();
-      $window.on("scroll resize", checkView);
+      $window.on("scroll", checkView);
 
-  /* contact form */
-  // scroll to form
-  $("a[title='contact']").click(function(e){
-    e.preventDefault();
-    var target = this.hash,
-        $target = $(target);
-    $(".mobile-nav").trigger("click");
-    $("html, body").stop().animate({
-      "scrollTop": $target.offset().top
-    }, 900, "swing", function(){
-      history.pushState(null, null, target);
-    });
-  });
-  // keep adaptive placeholders in place
-  $("input, textarea").change(function(){
-    var el = $(this);
-    el.val().trim().length ? el.addClass("focus") : el.removeClass("focus");
-  });
-  // initialize validation
-  $("#form").validate({
-    errorElement: "div",
-    errorPlacement: function(error, element) {
-      error.appendTo( element.parent("div") );
-    },
-    showErrors: function(errorMap, errorList) {
-      this.defaultShowErrors();
-      $.each(errorMap, function(key, value) {
-        var error = $("input[name='" + key + "']").nextAll("div");
-        if (error.text() !== value) {
-          error.html(value);
-        }
-      });
-    },
-    submitHandler: function(form, event) {
-      var hp = $("#website");
-      if (!hp.val().length) {
-        submitForm(form);
-      }
-    }
-  });
-  // submit form logic
-  var submitForm = function(form) {
-    var formMessages = $("#form-messages"),
-        formData = $(form).serialize();
-		$.ajax({
-			type: "POST",
-			url: $(form).attr("action"),
-			data: formData
-		})
-		.done(function(response) {
-			$(formMessages).removeClass("error").addClass("success");
-      $("input, textarea").removeClass("focus");
-			$(formMessages).hide().text(response).fadeIn();
-			$('#name, #email, #message').val("");
-      $(form).validate().resetForm();
-      setTimeout(function(){
-        $(formMessages).hide("slow");
-      }, 10000);
-		})
-		.fail(function(data) {
-			$(formMessages).removeClass("success").addClass("error");
-			if (data.responseText !== "") {
-				$(formMessages).hide().text(data.responseText).fadeIn();
-			} else {
-				$(formMessages).hide().text("Oops! Something went wrong and we couldn't send your message. Please try again.").fadeIn();
-			}
-		});
-  };
   /* detect flex support & redirect */
   // detect adapted from http://johanronsse.be/2016/01/03/simple-flexbox-check/
-  var detectFlex = function(){
-    var doc = document.documentElement,
-        f = doc.style.flex,
-        fw = doc.style.webkitFlex,
-        updateLink = "http://hannahsteindesigns.com/update";
+  const detectFlex = function(){
+    const doc = document.documentElement;
+    const f = doc.style.flex;
+    const fw = doc.style.webkitFlex;
+    const updateLink = "http://hannahsteindesigns.com/update";
+
+        // TO DO: can this be !f.length ?
         ( f === "" || fw === "" ) ? doc.className = "flex" : doc.className = "no-flex";
         if ( f === "" || fw === "" ) {
           doc.className = "flex";
